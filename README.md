275ab9bd41f96beb3923a98b274fe46d7e424994
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2023-03-24 00:23:55 -0400

    Add usage

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # imgterm
 
-Display images in terminal with true color.
+Display images in terminal.
+
+```
+Usage: imgterm [options] [files]
+Options
+    -w width
+        Pixel width of the image. Use screen width if set to 0
+    -h height
+        Pixel height of the image. Use screen height if set to 0
+    -p prercentage
+        Percentage of the image height to the screen height. Default 50
+    -r  Use raw size of the image
+    -8  Use 8-bit colors
+    -s  Use space
+    -?  Print this help
+```
--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include "stb_image.h"
 #include "stb_image_resize.h"
 
-int getWindowSize(int* rows, int* cols) {
+static int getWindowSize(int* rows, int* cols) {
     struct winsize ws;
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
         return -1;
@@ -17,7 +17,32 @@ int getWindowSize(int* rows, int* cols) {
     return 0;
 }
 
+static void usage(const char* prog) {
+    fprintf(stderr, "Usage: %s [options] [files]\n", prog);
+    fprintf(stderr, "Options\n");
+    fprintf(stderr, "    -w width\n");
+    fprintf(stderr,
+            "        Pixel width of the image. Use screen width if set to 0\n");
+    fprintf(stderr, "    -h height\n");
+    fprintf(
+        stderr,
+        "        Pixel height of the image. Use screen height if set to 0\n");
+    fprintf(stderr, "    -p prercentage\n");
+    fprintf(stderr,
+            "        Percentage of the image height to the screen height. "
+            "Default 50\n");
+    fprintf(stderr, "    -r  Use raw size of the image\n");
+    fprintf(stderr, "    -8  Use 8-bit colors\n");
+    fprintf(stderr, "    -s  Use space\n");
+    fprintf(stderr, "    -?  Print this help\n");
+}
+
 int main(int argc, char* argv[]) {
+    if (argc <= 0)
+        exit(EXIT_FAILURE);
+
+    const char* prog = argv[0];
+
     int th = -1, tw = -1;
     int p = 50;
     int raw_size = 0;
@@ -26,7 +51,7 @@ int main(int argc, char* argv[]) {
     SetColorFunc setColor = setTrueColor;
 
     int opt;
-    while ((opt = getopt(argc, argv, "w:h:p:rcs?")) != -1) {
+    while ((opt = getopt(argc, argv, "w:h:p:r8s?")) != -1) {
         switch (opt) {
             case 'w':
                 tw = atoi(optarg);
@@ -52,17 +77,18 @@ int main(int argc, char* argv[]) {
             case 'r':
                 raw_size = 1;
                 break;
-            case 'c':
+            case '8':
                 setColor = set256Color;
                 break;
             case 's':
                 use_space = 1;
                 break;
             default:
-                fprintf(stderr,
-                        "Usage: %s [-w width] [-h height] [-p percentage] [-r] "
-                        "[-c] [-s] files\n",
-                        argv[0]);
+                if (optopt == 0) {
+                    usage(prog);
+                    exit(EXIT_SUCCESS);
+                }
+                fprintf(stderr, "More info with \"%s -?\"\n", prog);
                 exit(EXIT_FAILURE);
         }
     }
@@ -127,7 +153,7 @@ int main(int argc, char* argv[]) {
                 setColor(pixels[x * w + y], 1);
                 if (use_space) {
                     printf("  ");
-                }else {
+                } else {
                     setColor(pixels[(x + 1) * w + y], 0);
                     printf("\u2584");
                 }
